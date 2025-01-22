7547002deb4e6a83e81741ff52c8896e101e3b8c
Jeff Britts <jbritts@adobe.com>
2025-01-22 15:45:25 -0600

    DEMOSYS-1138 Create base query for retrieving all information

---
+++ Model/Resolver/Instance/InstanceInfo.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright 2023 Adobe, Inc. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace MagentoEse\DataInstallGraphQl\Model\Resolver\Instance;
+
+use MagentoEse\DataInstallGraphQl\Model\Resolver\DataProvider\DataInstallLog as LogDataProvider;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use MagentoEse\DataInstallGraphQl\Model\Authentication;
+
+class InstanceInfo implements ResolverInterface
+{
+    /**
+     * @var LogDataProvider
+     */
+    private $logDataProvider;
+
+    /** @var Authentication */
+    protected $authentication;
+
+   /**
+    *
+    * @param LogDataProvider $logDataProvider
+    * @param Authentication $authentication
+    * @return void
+    */
+    public function __construct(
+        LogDataProvider $logDataProvider,
+        Authentication $authentication
+    ) {
+        $this->logDataProvider = $logDataProvider;
+        $this->authentication = $authentication;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        $this->authentication->authorize();
+
+        $logData = $this->logDataProvider->getInstalledDataPacks();
+        
+        return [
+             'datapacks' => $logData,
+        ];
+    }
+}
